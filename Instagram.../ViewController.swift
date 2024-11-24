6dc79c709a732b4b7d3b3d7a8d2fa2ca367ca3fe
Sri Hariharan <sri.hariharan@zohocorp.com>
2024-11-24 18:25:26 +0530

    Initial commit with iOS project files

---
+++ Instagram.../ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram...
+//
+//  Created by sid-zstch1431 on 24/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
